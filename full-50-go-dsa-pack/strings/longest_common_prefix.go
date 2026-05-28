f474f67913e01b9e6add0e6e54bcc297e53f39ef
Siva Siddamoorthy <siva.siddamoorthy3@gmail.com>
2026-05-28 21:46:52 +0530

    updated string dsa longest common prefix code

--- full-50-go-dsa-pack/strings/longest_common_prefix.go
+++ full-50-go-dsa-pack/strings/longest_common_prefix.go
@@ -1,6 +1,29 @@
-
 package strings
 
-func Solve() {
-    // TODO: Implement
+import "strings"
+
+func LongestCommonPrefix(strs []string) string {
+	// Handle edge cases
+	if len(strs) == 0 {
+		return ""
+	}
+	if len(strs) == 1 {
+		return strs[0]
+	}
+
+	// Start with the first string as the initial prefix
+	prefix := strs[0]
+
+	// Iterate through the rest of the strings
+	for i := 1; i < len(strs); i++ {
+		// Reduce the prefix until it matches the start of the current string
+		for !strings.HasPrefix(strs[i], prefix) {
+			prefix = prefix[:len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+
+	return prefix
 }
